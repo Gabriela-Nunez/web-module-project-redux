4ec6821c30f83ee7df31bdbf9c69122cab0524f8
Gabby Nunez <gabbynunez@Gabbys-MacBook-Pro.local>
2023-01-04 12:23:02 -0600

    Build out favorites actions and MVP tasks were met.

--- src/reducers/favoritesReducer.js
+++ src/reducers/favoritesReducer.js
@@ -1,3 +1,4 @@
+import { ADD_FAVORITE, REMOVE_FAVORITE, TOGGLE_FAVORITES } from "../actions/favoritesActions";
 import movies from "../data";
 
 const initialState = {
@@ -7,6 +8,21 @@ const initialState = {
 
 const favoritesReducer = (state = initialState, action) => {
   switch(action.type) {
+    case TOGGLE_FAVORITES:
+      return {
+        ...state,
+        displayFavorites: !state.displayFavorites
+      }
+      case ADD_FAVORITE:
+        return {
+          ...state,
+          favorites: [...state.favorites, action.payload]
+        }
+        case REMOVE_FAVORITE:
+          return {
+            ...state,
+            favorites: state.favorites.filter(movie => (movie.id !== action.payload))
+          }
     default:
       return state;
   }
